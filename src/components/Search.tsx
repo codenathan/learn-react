f50b9f3c235e711bb1e602bf1c509ade41d28ca9
S Nathan <codenathan@codenathan.com>
2025-05-01 21:34:43 +0100

    input focus on reset

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,5 @@
 import { Delete } from "lucide-react";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useRef } from "react";
 
 export function Search({
     searchQuery,
@@ -8,6 +8,8 @@ export function Search({
     searchQuery: string;
     setSearchQuery: Dispatch<SetStateAction<string>>;
 }) {
+
+    const inputRef = useRef(null)
     return (
         <div>
         <label htmlFor="search" className="font-medium">
@@ -15,6 +17,7 @@ export function Search({
         </label>
         <div className="mt-2 flex items-center gap-4">
           <input
+            ref={inputRef}
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
             placeholder="playful..."
@@ -23,7 +26,10 @@ export function Search({
             type="text"
             className="w-full max-w-80 bg-white px-4 py-2 ring ring-black/5 focus:ring-2 focus:ring-cyan-500 focus:outline-none"
           />
-          <button onClick={() => setSearchQuery("")} className="inline-block rounded bg-cyan-300 px-4 py-2 !pr-3 !pl-2.5 font-medium text-cyan-900 hover:bg-cyan-200 focus:ring-2 focus:ring-cyan-500 focus:outline-none">
+          <button onClick={() => {
+            setSearchQuery("");
+            inputRef.current.focus();
+          }} className="inline-block rounded bg-cyan-300 px-4 py-2 !pr-3 !pl-2.5 font-medium text-cyan-900 hover:bg-cyan-200 focus:ring-2 focus:ring-cyan-500 focus:outline-none">
             <Delete />
             
           </button>
